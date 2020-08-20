0ab63cf0d29d3fed00e87e4163e47f25f92eabaa
kostya <matvienkoigor956@gmail.com>
2020-08-20 19:01:33 +0300

    added toggle button

--- js/script.js
+++ js/script.js
@@ -1,27 +1,60 @@
 // -------------------------------------------------------- fixed menu --------------------------------------------
-window.onscroll = function() { myFunction() };
+$(window).scroll(() => {
+    let topbarHeight = $('.top-bar').outerHeight();
+    let headerHeight = $('header').outerHeight();
 
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
+    if ($(window).scrollTop() > topbarHeight) {
+        $('header').addClass("sticky");
+        $('.carousel-area').css('margin-top', headerHeight);
     } else {
-        header.classList.remove("sticky");
-    }
-}
-// ======================================================== / fixed menu ==========================================
-// -------------------------------------------------------- scroll to section -------------------------------------
-$(function() {
+        $('header').removeClass("sticky");
+        $('.carousel-area').css('margin-top', 0);
 
-    $('.some_link').on('click', function(e) {
-        $('html,body').stop().animate({ scrollTop: $($(this).attr('href')).offset().top }, 300);
-        e.preventDefault();
-    });
+    }
+});
+// ======================================================== / fixed menu ==========================================
+// -------------------------------------------------------- toggle drop menu --------------------------------------
+let header = $("#header");
+let topbar = $(".top-bar");
+
+
+$("#toggle_button").click(() => {
+    toggleClass(header, "open_menu");
+    topbar.removeClass("right_panel_open");
+});
+
+$("#right_panel_button").click(() => {
+    toggleClass(topbar, "right_panel_open");
+    header.removeClass("open_menu");
+
+});
+
+function toggleClass(elem, class_name) {
+    elem.hasClass(class_name) ? elem.removeClass(class_name) : elem.addClass(class_name);
+}
+
+// ======================================================== / toggle drop menu ====================================
+
+// -------------------------------------------------------- scroll to section -------------------------------------
+
+$('.nav_link').on('click', function(e) {
+    $('html,body').stop().animate({ scrollTop: $($(this).attr('href')).offset().top }, 1200);
+    e.preventDefault();
+
+    if (!$(this).hasClass('active')) {
+        $('.nav_link').removeClass('active');
+
+        $(this).addClass('active');
+    }
+    header.removeClass("open_menu");
+
 });
 
 // ======================================================== / scroll to section ===================================
+// -------------------------------------------------------- add/remove active class -------------------------------------
+
+
+// ======================================================== / add/remove active class ===================================
 // -------------------------------------------------------- BUTTON scroll to top ----------------------------------
 
 $(function() {
